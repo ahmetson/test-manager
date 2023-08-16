091af256744b50ff624646ab505a15193b34ac62
Medet Ahmetson <admin@blocklords.io>
2023-08-16 22:46:37 +0300

    Setup N second living server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ahmetson/test-manager
 
 go 1.21rc2
+
+require github.com/pebbe/zmq4 v1.2.10 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"github.com/pebbe/zmq4"
+	"time"
+)
+
+const N = 2
+
+// For N seconds, it imitates the SDS handler as busy.
+// For testing dep_manager.Running() command.
 func main() {
-	println("hello world!")
+
+	socket, err := zmq4.NewSocket(zmq4.REP)
+	if err != nil {
+		panic(err)
+	}
+
+	// port = 8081
+	err = socket.Bind("tpc://*:8081")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Second * N)
+
+	err = socket.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	println("Server exited successfully")
 }
