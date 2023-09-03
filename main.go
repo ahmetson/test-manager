4e9eb5619e913c366e568208f2153d7b212b1282
Medet Ahmetson <ahmetson@zoho.com>
2023-09-03 20:38:29 +0400

    the server receives and returns the messages

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/ahmetson/common-lib/data_type/key_value"
+	"github.com/ahmetson/common-lib/message"
 	"github.com/pebbe/zmq4"
 	"time"
 )
@@ -22,7 +24,46 @@ func main() {
 		panic(err)
 	}
 
-	time.Sleep(time.Second * N)
+	poller := zmq4.NewPoller()
+	poller.Add(socket, zmq4.POLLIN)
+
+	currentTime := time.Now()
+	endTime := currentTime.Add(time.Second * N)
+
+	for {
+		polled, err := poller.Poll(time.Millisecond)
+		if err != nil {
+			panic(err)
+		}
+
+		currentTime = currentTime.Add(time.Millisecond)
+		if currentTime.UnixMilli() >= endTime.UnixMilli() {
+			break
+		}
+
+		if len(polled) == 0 {
+			continue
+		}
+
+		_, err = socket.RecvMessage(0)
+		if err != nil {
+			panic(err)
+		}
+
+		reply := message.Reply{
+			Status:     message.OK,
+			Parameters: key_value.Empty(),
+			Message:    "",
+		}
+		replyStr, err := reply.String()
+		if err != nil {
+			panic(err)
+		}
+		_, err = socket.SendMessage(replyStr)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	err = socket.Close()
 	if err != nil {
