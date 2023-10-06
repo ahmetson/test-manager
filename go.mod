9374415eb79e99bfd297a4312c9fbb4ba3ee5b68
Medet Ahmetson <ahmetson@zoho.com>
2023-10-06 21:20:41 +0700

    Imitate the proxy

--- go.mod
+++ go.mod
@@ -4,14 +4,18 @@ go 1.21rc2
 
 require (
 	github.com/ahmetson/common-lib v0.0.0-20230825195825-e7b69549b9a1
+	github.com/ahmetson/os-lib v0.0.0-20231002120043-1c5bcc771249
 	github.com/pebbe/zmq4 v1.2.10
 )
 
 require (
+	github.com/ahmetson/client-lib v0.0.0-20231004170410-5cb1c2b5a2cd // indirect
+	github.com/ahmetson/datatype-lib v0.0.0-20230927201942-0cc58292a7a3 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/ethereum/go-ethereum v1.10.25 // indirect
-	github.com/google/uuid v1.2.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,36 +2,47 @@ package main
 
 import (
 	"fmt"
-	"github.com/ahmetson/common-lib/data_type/key_value"
-	"github.com/ahmetson/common-lib/message"
+	clientConfig "github.com/ahmetson/client-lib/config"
+	"github.com/ahmetson/datatype-lib/data_type/key_value"
+	"github.com/ahmetson/datatype-lib/message"
+	"github.com/ahmetson/os-lib/arg"
 	"github.com/pebbe/zmq4"
+	"os"
 	"time"
 )
 
-const N = 2
+const PortFlag = "port"
+const ManagerPortFlag = "manager-port"
+const IdFlag = "id"
+const UrlFlag = "url"
+const ParentFlag = "parent"
 
-// For N seconds, it imitates the SDS handler as busy.
-// For testing dep_manager.Running() command.
-func main() {
+//const ParentFlag = "parent"
 
+func runHandler(port string) {
 	socket, err := zmq4.NewSocket(zmq4.REP)
 	if err != nil {
 		panic(err)
 	}
 
-	err = socket.Bind("tcp://*:6000")
+	err = socket.Bind(fmt.Sprintf("tcp://*:%s", port))
 	if err != nil {
-		println("bind error")
+		println("runHandler: bind error")
 		panic(err)
 	}
+	defer func() {
+		err := socket.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
 
 	poller := zmq4.NewPoller()
 	poller.Add(socket, zmq4.POLLIN)
 
 	currentTime := time.Now()
-	endTime := currentTime.Add(time.Second * N)
 
-	fmt.Printf("current time: %v, end time: %v\n", currentTime, endTime)
+	fmt.Printf("runHandler: current time: %v, server running on port %s\n", currentTime, port)
 
 	for {
 		polled, err := poller.Poll(time.Millisecond)
@@ -39,29 +50,80 @@ func main() {
 			panic(err)
 		}
 
-		currentTime = time.Now()
-		if currentTime.UnixMilli() >= endTime.UnixMilli() {
+		if len(polled) == 0 {
+			continue
+		}
+
+		raw, err := socket.RecvMessage(0)
+		if err != nil {
+			panic(err)
+		}
+
+		req, err := message.NewReq(raw)
+		if err != nil {
+			panic(err)
+		}
+
+		reply := req.Ok(key_value.New().Set("command", req.CommandName()))
+		replyStr := reply.String()
+		_, err = socket.SendMessage(replyStr)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func runManager(port string) {
+	closeFlag := false
+
+	socket, err := zmq4.NewSocket(zmq4.REP)
+	if err != nil {
+		panic(err)
+	}
+
+	err = socket.Bind(fmt.Sprintf("tcp://*:%s", port))
+	if err != nil {
+		println("runManager: bind error")
+		panic(err)
+	}
+
+	poller := zmq4.NewPoller()
+	poller.Add(socket, zmq4.POLLIN)
+
+	currentTime := time.Now()
+
+	fmt.Printf("runManager: current time: %v, server running on port %s\n", currentTime, port)
+
+	for {
+		if closeFlag {
 			break
+		}
+
+		polled, err := poller.Poll(time.Millisecond)
+		if err != nil {
+			panic(err)
 		}
 
 		if len(polled) == 0 {
 			continue
 		}
 
-		_, err = socket.RecvMessage(0)
+		raw, err := socket.RecvMessage(0)
 		if err != nil {
 			panic(err)
 		}
 
-		reply := message.Reply{
-			Status:     message.OK,
-			Parameters: key_value.Empty(),
-			Message:    "",
-		}
-		replyStr, err := reply.String()
+		req, err := message.NewReq(raw)
 		if err != nil {
 			panic(err)
 		}
+
+		if req.CommandName() == "close" {
+			closeFlag = true
+		}
+
+		reply := req.Ok(key_value.New().Set("command", req.CommandName()))
+		replyStr := reply.String()
 		_, err = socket.SendMessage(replyStr)
 		if err != nil {
 			panic(err)
@@ -70,9 +132,65 @@ func main() {
 
 	err = socket.Close()
 	if err != nil {
-		println("close error")
+		println("runManager: close error")
 		panic(err)
 	}
 
-	fmt.Printf("Server exited successfully: %v, now: %v\n", currentTime, time.Now())
+	fmt.Printf("Proxy exited successfully: %v, now: %v\n", currentTime, time.Now())
+	os.Exit(0)
+}
+
+// For N seconds, it imitates the SDS handler as busy.
+// For testing dep_manager.Running() command.
+func main() {
+	port := "6000" // default port
+	if arg.FlagExist(PortFlag) {
+		port = arg.FlagValue(PortFlag)
+	}
+	managerPort := "6001"
+	if arg.FlagExist(ManagerPortFlag) {
+		managerPort = arg.FlagValue(ManagerPortFlag)
+	}
+	if !arg.FlagExist(IdFlag) {
+		panic("no id flag")
+	}
+	if !arg.FlagExist(UrlFlag) {
+		panic("no url flag")
+	}
+	if !arg.FlagExist(ParentFlag) {
+		panic("no parent flag")
+	}
+	id := arg.FlagValue(IdFlag)
+	url := arg.FlagValue(UrlFlag)
+	parentRaw := arg.FlagValue(ParentFlag)
+	parentKv, err := key_value.NewFromString(parentRaw)
+	if err != nil {
+		panic(err)
+	}
+
+	var parentConfig clientConfig.Client
+	err = parentKv.Interface(&parentConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	//parentConfig.UrlFunc(clientConfig.Url)
+	//parent, err := client.New(&parentConfig)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	fmt.Printf("proxy imitating with id='%s', url='%s', port='%s', managerPort='%s'\n", id, url, port, managerPort)
+
+	fmt.Printf("proxy generated a configuration...notify the parent\n")
+	fmt.Printf("fetch from the parent the proxy chain\n")
+	fmt.Printf("fetch from the parent the proxy units\n")
+
+	time.Sleep(time.Second)
+
+	fmt.Printf("proxy imitated that all of it's proxies are set\n")
+	fmt.Printf("notify parent that PROXY READY")
+
+	go runHandler(port)
+	runManager(managerPort)
 }
